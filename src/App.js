c51402c7ab40de30a59c1cb04014592cf1e0fa51
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-15 12:40:59 +0530

    feat: add card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components";
 import logo from "./logo.png";
+import { VideoListing } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<VideoListing />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/videoListing/VideoListing.jsx
@@ -0,0 +1,13 @@
+import { Card } from "../../components";
+
+const VideoListing = () => {
+  return (
+    <div className="car-container">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export { VideoListing };

0c2e8457a60386056122cd6a2bd681c08abe7fca
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-17 13:21:32 +0530

    feat: add auth-context

---
+++ src/context/auth-context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+const initialState = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+const AuthProvider = ({ children }) => {
+  const [authState, authDispatch] = useReducer(authReducer, initialState);
+  return (
+    <AuthContext.Provider value={{ authState, authDispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };

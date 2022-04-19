d3557269704c647332ec97d067514d77bc710b7f
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-19 10:18:33 +0530

    feat: add getLikedVideo service

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 export { loginService } from "./authentication/login.service";
 export { signupService } from "./authentication/signup.service";
 export { getVideosService } from "./videos/getVideos.service";
+export { getLikedVideoService } from "./likedVideos/getLikedVideo.service";
---
+++ src/services/likedVideos/getLikedVideo.service.js
@@ -0,0 +1,12 @@
+const getLikedVideoService = (token) => {
+  const url = "/api/user/likes";
+  const config = {
+    headers: {
+      authorization: token,
+    },
+  };
+  const response = axios.get(url, config);
+  return response;
+};
+
+export { getLikedVideoService };

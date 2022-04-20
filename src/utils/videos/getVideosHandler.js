886f9719b6f8b72bc00e6001c8e35856c6790191
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-20 10:40:26 +0530

    feat:add getVideos handler

--- src/utils/videos/getVideosHandler.js
+++ src/utils/videos/getVideosHandler.js
@@ -1,8 +1,14 @@
 import { getVideosService } from "../../services";
 
-const getVideosHandler = async () => {
+const getVideosHandler = async (setExploreVideos) => {
   try {
     const response = await getVideosService();
+    if (response.status === 200) {
+      const {
+        data: { videos },
+      } = response;
+      setExploreVideos((prevVideos) => [...prevVideos, ...videos]);
+    }
   } catch (error) {
     console.log(error);
   }

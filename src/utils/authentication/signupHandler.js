db45900336814f7c643baa42f0ed7d3482672fb4
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-21 10:39:36 +0530

    fix:fixed bug in signup

--- src/utils/authentication/signupHandler.js
+++ src/utils/authentication/signupHandler.js
@@ -1,11 +1,12 @@
 import { signupService } from "../../services";
 
-const signupHandler = async ({ authDispatch, newUser }) => {
+const signupHandler = async ({ authDispatch, newUser, navigate }) => {
   try {
-    const response = await signupService();
+    const response = await signupService(newUser);
     if (response.status === 201) {
       const { encodedToken: token, createdUser: user } = response.data;
       authDispatch({ type: "SIGNUP", payload: { token, user } });
+      navigate("/");
     }
   } catch (error) {
     console.error(error);

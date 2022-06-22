abb2bb2ae77575c2e866ac5bc20a389d5c4ba1ef
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-06-22 11:30:30 +0530

    feat:add signup thunk

--- src/redux/thunk/authThunk.js
+++ src/redux/thunk/authThunk.js
@@ -6,9 +6,20 @@ export const login = createAsyncThunk("auth/login", async (userCred) => {
     const url = "/api/auth/login";
     const data = userCred;
     const response = await axios.post(url, data);
-    console.log("response from thunk:", response);
     const { encodedToken: token, foundUser: user } = response.data;
     // whatever i return from thunk, is passed as payload inside action object to extra reducers function
+    return { token, user, status: response.status };
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const signup = createAsyncThunk("auth/signup", async () => {
+  try {
+    const url = "/api/auth/signup";
+    const data = newUser;
+    const response = await axios.post(url, data);
+    const { encodedToken: token, createdUser: user } = response.data;
     return { token, user, status: response.status };
   } catch (error) {
     console.error(error);
